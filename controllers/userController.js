a3f8655a04b1c85ec7be7c5700885df083683258
unknown <sebastian.1701811396@ucaldas.edu.co>
2024-12-26 13:00:02 -0500

    Creación de las rutas para configurar la lógica dentro del modelo de usuario

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// Route for user register
+const registerUser = async (req, res) => {
+    res.json({msg: 'API funcionando'})
+}
+
+// Route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
