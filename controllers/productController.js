a67138d5125f2f726021e78d8fbf0c76095a9278
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-01-03 12:06:11 -0500

    Creación de la lógica para la API de mostrar un solo producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -67,6 +67,15 @@ const removeProduct = async (req, res) => {
 };
 
 // function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+    try {
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+};
 
 export { listProducts, addProduct, removeProduct, singleProduct };
