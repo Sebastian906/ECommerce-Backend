a3e9ef3ea0e4908901fc6fc190b67dad3a0dc431
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-03-11 13:39:40 -0500

    Se crea la API correspondiente al método de pago de Razorpay

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from 'stripe'
+import razorpay from 'razorpay'
 
 // global variables
 const currency = 'dollar'
@@ -8,6 +9,11 @@ const deliveryCharge = 10
 
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+})
 
 // Placing orders using COD Method
 const placeOrder = async (req, res) => {
@@ -101,7 +107,35 @@ const verifyStripe = async (req, res) => {
 
 // Placing orders using Razorpay Method
 const placeOrderRazorpay = async (req, res) => {
-
+    try {
+        const { userId, items, amount, address } = req.body
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"Razorpay",
+            payment: false,
+            date: Date.now()
+        }
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+        const options = {
+            amount: amount * 100,
+            currency: currency.toUpperCase(),
+            receipt: newOrder._id.toString()
+        }
+        await razorpayInstance.orders.create(options, (error, order) => {
+            if (error) {
+                console.log(error);
+            return res.json({ success: false, message: error })
+            }
+            res.json({ success: true, order });
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({ success:false,message:error.message });
+    }
 }
 
 // All Orders data for Admin Panel
